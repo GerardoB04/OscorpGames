5023689d1a6a4ffec37429b169bb6f8ec9f1eb3f
Croc-ker <123499932+Croc-ker@users.noreply.github.com>
2023-11-08 18:36:10 -0700

    Foundation for the project. includes: -Main menu - 3 empty forms for the 3 games.

---
+++ Main.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace OscorpGames
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(212, 465);
+            button1.Name = "button1";
+            button1.Size = new Size(188, 85);
+            button1.TabIndex = 0;
+            button1.Text = "Game3";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 40F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(254, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(481, 106);
+            label1.TabIndex = 1;
+            label1.Text = "MINIGAMES";
+            label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(212, 249);
+            button2.Name = "button2";
+            button2.Size = new Size(188, 85);
+            button2.TabIndex = 2;
+            button2.Text = "Game1";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(603, 249);
+            button3.Name = "button3";
+            button3.Size = new Size(188, 85);
+            button3.TabIndex = 3;
+            button3.Text = "Game2";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(603, 465);
+            button4.Name = "button4";
+            button4.Size = new Size(188, 85);
+            button4.TabIndex = 4;
+            button4.Text = "Game4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(883, 761);
+            button5.Name = "button5";
+            button5.Size = new Size(112, 34);
+            button5.TabIndex = 5;
+            button5.Text = "Exit";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 612);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 25);
+            label2.TabIndex = 6;
+            label2.Text = "CREATED BY:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 762);
+            label3.Name = "label3";
+            label3.Size = new Size(113, 25);
+            label3.TabIndex = 7;
+            label3.Text = "CREATED BY:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 737);
+            label4.Name = "label4";
+            label4.Size = new Size(173, 25);
+            label4.TabIndex = 8;
+            label4.Text = "GERARDO BARAJAS";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 712);
+            label5.Name = "label5";
+            label5.Size = new Size(117, 25);
+            label5.TabIndex = 9;
+            label5.Text = "CURTIS LANE";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 687);
+            label6.Name = "label6";
+            label6.Size = new Size(149, 25);
+            label6.TabIndex = 10;
+            label6.Text = "WYATT CROCKER";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 662);
+            label7.Name = "label7";
+            label7.Size = new Size(163, 25);
+            label7.TabIndex = 11;
+            label7.Text = "SURYA NANDEESH";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(12, 637);
+            label8.Name = "label8";
+            label8.Size = new Size(133, 25);
+            label8.TabIndex = 12;
+            label8.Text = "COLLIN GREISS";
+            label8.Click += label8_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1007, 807);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+    }
+}
