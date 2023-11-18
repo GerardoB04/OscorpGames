f0abbbc4f498d5c539dd0f76fea63f25ea0dee31
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2023-11-17 18:34:01 -0700

    Make game buttons functional

--- Menu/Menu.cs
+++ Menu/Menu.cs
@@ -1,34 +1,39 @@
+using OscorpGames.Pac_Man;
+
 namespace OscorpGames {
-    public partial class Menu : Form
-    {
-        public Menu()
-        {
-            InitializeComponent();
-        }
+	public partial class Menu : Form {
+		public Menu() {
+			InitializeComponent();
+		}
 
-        private void exitButton_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+		private void exitButton_Click(object sender, EventArgs e) {
+			Close();
+		}
 
-        private void MainMenu_Load(object sender, EventArgs e)
-        {
-            //
-        }
+		private void MainMenu_Load(object sender, EventArgs e) {
+			//
+		}
 
-        private void OpenGame(Form game)
-        {
-            game.ShowDialog();
-        }
+		private void OpenGame(Form game) {
+			this.Hide();
+			game.ShowDialog();
+			this.Show();
+		}
 
-        private void creatorLabel2_Click(object sender, EventArgs e)
-        {
+		private void gameOneButton_Click(object sender, EventArgs e) {
+			OpenGame(new Minesweeper());
+		}
 
-        }
+		private void gameTwoButton_Click(object sender, EventArgs e) {
+			//OpenGame(new Snake());
+		}
 
-        private void gameOneButton_Click(object sender, EventArgs e)
-        {
+		private void gameThreeButton_Click(object sender, EventArgs e) {
+			OpenGame(new Pac_Man.Pac_Man());
+		}
 
-        }
-    }
+		private void gameFourButton_Click(object sender, EventArgs e) {
+			OpenGame(new Tetris());
+		}
+	}
 }
