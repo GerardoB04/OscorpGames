8f250338b2b611f713c2672a36d2dddf1afa25fd
Gerardo Barajas <123435420+GerardoB04@users.noreply.github.com>
2023-11-29 16:34:46 -0700

    added leaderboard form for dialog

---
+++ Menu/Leaderboard.cs
@@ -0,0 +1,10 @@
+﻿namespace OscorpGames.Menu
+{
+    public partial class Leaderboard : Form
+    {
+        public Leaderboard(string game)
+        {
+            InitializeComponent();
+        }
+    }
+}
