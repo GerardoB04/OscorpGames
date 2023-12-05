ea0d0736434a4d64f40ac2d181a873d7d2f75365
Croc-ker <123499932+Croc-ker@users.noreply.github.com>
2023-12-04 18:45:01 -0700

    Ghost collisions. Also the player gif now properly loops.

--- Menu/Leaderboard.Designer.cs
+++ Menu/Leaderboard.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Leaderboard";
+            SuspendLayout();
+            // 
+            // Leaderboard
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1058, 702);
+            Name = "Leaderboard";
+            Text = "Leaderboard";
+            ResumeLayout(false);
         }
 
         #endregion
